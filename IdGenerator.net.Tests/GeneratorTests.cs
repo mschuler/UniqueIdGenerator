08dbec23f29e9dcc1a1fbe52e1b7cfb470828326
mschuler <michael@schuler-hunziker.ch>
2013-11-19 14:44:54 +0100

    initial commit

---
+++ IdGenerator.net.Tests/GeneratorTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IdGenerator.Net.Tests
+{
+    public class GeneratorTests
+    {
+        [Fact]
+        public void GetThousands()
+        {
+            const int Number = 100000;
+
+            var stopwatch = Stopwatch.StartNew();
+            var ids = GetIds(0, Number);
+            stopwatch.Stop();
+
+            Console.WriteLine("Duration to generate {1} ids: {0} ms", stopwatch.ElapsedMilliseconds, ids.Count);
+            Console.WriteLine("Number of ids generated in 1 ms: {0}", ids.Count / stopwatch.ElapsedMilliseconds);
+            Console.WriteLine();
+
+            var unique = new HashSet<string>(StringComparer.Ordinal);
+            foreach (var id in ids)
+            {
+                Assert.False(unique.Contains(id), id);
+                unique.Add(id);
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(ids[i]);
+            }
+        }
+
+        [Fact]
+        public void GetWithHundredMachines()
+        {
+            const int Number = 10000;
+            const int Machines = 100;
+            var lists = new IList<string>[Machines];
+
+            var stopwatch = Stopwatch.StartNew();
+            Parallel.For(0, Machines, m => { lists[m] = GetIds((short)m, Number); });
+            stopwatch.Stop();
+
+            var ids = lists.SelectMany(l => l).ToList();
+
+            Console.WriteLine("Duration to generate {1} ids: {0} ms", stopwatch.ElapsedMilliseconds, ids.Count);
+            Console.WriteLine("Number of ids generated in 1 ms: {0}", ids.Count / stopwatch.ElapsedMilliseconds);
+            Console.WriteLine();
+
+            var unique = new HashSet<string>(StringComparer.Ordinal);
+            foreach (var id in ids)
+            {
+                Assert.False(unique.Contains(id), id);
+                unique.Add(id);
+            }
+        }
+
+        [Fact]
+        public void GetValue()
+        {
+            var target = new byte[8];
+            var generator = new Generator(1023, DateTime.Today);
+
+            generator.WriteValuesToByteArray(target, 4398046511103, 4095);
+            Assert.Equal("1111111111111111111111111111111111111111111111111111111111111111", GetString(target));
+
+            generator.WriteValuesToByteArray(target, 4398046511103, 4094);
+            Assert.Equal("1111111111111111111111111111111111111111111111111111111111111110", GetString(target));
+
+            generator.WriteValuesToByteArray(target, 4398046511102, 4095);
+            Assert.Equal("1111111111111111111111111111111111111111101111111111111111111111", GetString(target));
+
+            generator = new Generator(1022, DateTime.Today);
+            generator.WriteValuesToByteArray(target, 4398046511103, 4095);
+            Assert.Equal("1111111111111111111111111111111111111111111111111110111111111111", GetString(target));
+        }
+
+        [Fact]
+        public void CollisionTest()
+        {
+            Func<string, string> bits = id => GetString(Convert.FromBase64String(id));
+            Func<string, long> ms = id => Convert.ToInt64(GetString(Convert.FromBase64String(id)).Substring(0, 42), 2);
+            Func<string, short> mid = id => Convert.ToInt16(GetString(Convert.FromBase64String(id)).Substring(42, 10), 2);
+            Func<string, short> seq = id => Convert.ToInt16(GetString(Convert.FromBase64String(id)).Substring(52, 12), 2);
+            Func<long, string> ts = l => TimeSpan.FromMilliseconds(l).ToString("g", CultureInfo.InvariantCulture);
+
+            Action<string> print = id => Console.WriteLine("{0} - {1} - {2} - {3}", bits(id), ts(ms(id)), mid(id), seq(id));
+
+            print("AACxgFbAEAA=");
+            print("AAC3UcJAMAA=");
+            print("AAC3a4sAMAA=");
+        }
+
+        private static string GetString(byte[] bytes)
+        {
+            return string.Concat(bytes.SelectMany(y => Convert.ToString(y, 2).PadLeft(8, '0')));
+        }
+
+        private IList<string> GetIds(short machineId, int number)
+        {
+            var generator = new Generator(machineId, DateTime.Today);
+            var ids = new List<string>(number);
+
+            for (int i = 0; i < number; i++)
+            {
+                ids.Add(generator.Next());
+            }
+
+            return ids;
+        }
+    }
+}
---
+++ IdGenerator.net/Generator.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace IdGenerator.Net
+{
+    public class Generator
+    {
+        private readonly byte[] _buffer = new byte[8];
+        private readonly byte[] _timeBytes = new byte[8];
+        private readonly byte[] _idBytes = new byte[2];
+        private readonly byte[] _sequenceBytes = new byte[2];
+
+        private readonly DateTime _start;
+
+        private short _sequence;
+        private long _previousTime;
+
+        public Generator(short machineId, DateTime start)
+        {
+            if (machineId < 0 || machineId > 1023)
+            {
+                throw new ArgumentException("machineId must be between 0 and 1023.", "machineId");
+            }
+            if (start > DateTime.Today)
+            {
+                throw new ArgumentException("start date must not be in the future.", "start");
+            }
+
+            CalculateIdBytes(machineId);
+            _start = start;
+        }
+
+        public string Next()
+        {
+            SpinToNextSequence();
+
+            WriteValuesToByteArray(_buffer, _previousTime, _sequence);
+            return Convert.ToBase64String(_buffer, Base64FormattingOptions.None);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("IdGenerator-{0:0000}", BitConverter.ToUInt16(new[] { _idBytes[0], _idBytes[1] }, 0));
+        }
+
+        internal unsafe void WriteValuesToByteArray(byte[] target, long time, short sequence)
+        {
+            fixed (byte* numPtr = target)
+            {
+                *(long*)numPtr = 0;
+            }
+
+            fixed (byte* numPtr = _timeBytes)
+            {
+                *(long*)numPtr = time << 22;
+            }
+
+            fixed (byte* numPtr = _sequenceBytes)
+            {
+                *(short*)numPtr = sequence;
+            }
+
+            WriteValuesToByteArray(target, _timeBytes, _idBytes, _sequenceBytes);
+        }
+
+        private unsafe void CalculateIdBytes(short id)
+        {
+            fixed (byte* numPtr = _idBytes)
+            {
+                *(short*)numPtr = (short)(id << 4);
+            }
+        }
+
+        private void WriteValuesToByteArray(byte[] target, byte[] time, byte[] id, byte[] sequence)
+        {
+            ////                                                 1234567890123456789012
+            //// time: 1111111111111111111111111111111111111111110000
+            //// id:                                           0011111111110000
+            //// seq:                                                  0000111111111111
+            ////
+            //// pos:  0         1         2         3         4         5         6
+            //// byte: 0       1       2       3       4       5       6       7
+
+            target[0] = (byte)(target[0] | time[7]);
+            target[1] = (byte)(target[1] | time[6]);
+            target[2] = (byte)(target[2] | time[5]);
+            target[3] = (byte)(target[3] | time[4]);
+            target[4] = (byte)(target[4] | time[3]);
+            target[5] = (byte)(target[5] | time[2]);
+
+            target[5] = (byte)(target[5] | id[1]);
+            target[6] = (byte)(target[6] | id[0]);
+
+            target[6] = (byte)(target[6] | sequence[1]);
+            target[7] = (byte)(target[7] | sequence[0]);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void SpinToNextSequence()
+        {
+            long time;
+
+            while (_previousTime == (time = GetTime()) && _sequence >= 4096)
+            {
+                Thread.Sleep(1);
+            }
+
+            _sequence = (short)((_previousTime != time) ? 0 : _sequence + 1);
+
+            _previousTime = time;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private long GetTime()
+        {
+            return (long)(DateTime.UtcNow - _start).TotalMilliseconds;
+        }
+    }
+}
